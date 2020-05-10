00eee383d15cb74f0071ae4ada6b7832300daca1
User <cory.bostock@gmail.com>
2020-05-10 19:45:06 +1000

    Modified multi bot launch file to be suitable for our project. Cleaned up the main to call the launch file and set break points in the code to read the workspace and command window

--- turtleBotMain.m
+++ turtleBotMain.m
@@ -1,15 +1,17 @@
 %% Authors Cory Bostock Sabrina Emanouel
 % Prep the workspace
-
 close all
 set(0,'DefaultFigureWindowStyle','docked')
 clc
 clf
 clear
 
+% Init ROS
 rosshutdown;
 rosinit;
 rostopic list;
-disp('Run roslaunch in the terminal then press enter to continue: ')
+disp('Run roslaunch files in order, then continue the Matlab script: ')
+disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
+dbstop at 15 in turtleBotMain;
 rostopic list;
 rosshutdown;
