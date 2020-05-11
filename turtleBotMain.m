bea74ac79b4a02f6e13718a55f4a490320c455d2
User <cory.bostock@gmail.com>
2020-05-11 21:33:55 +1000

    Set up subscriber and publisher. Verified guider moves

--- turtleBotMain.m
+++ turtleBotMain.m
@@ -7,11 +7,25 @@ clf
 clear
 
 % Init ROS
-rosshutdown;
-rosinit;
-rostopic list;
-disp('Run roslaunch files in order, then continue the Matlab script: ')
-disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
-dbstop at 15 in turtleBotMain;
-rostopic list;
-rosshutdown;
+% rosshutdown;
+% rosinit;                                                                        % makes a ROS node and allows comms to ROS
+% rostopic list;
+% disp('Run roslaunch files in order, then continue the Matlab script: ')
+% disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
+% dbstop at 16 in turtleBotMain;
+% rostopic list;
+
+% Subscribing and Publishing ROS Topics
+guiderOdom = rossubscriber('/guider/odom');
+guiderScan = rossubscriber('/guider/scan');
+followerOdom = rossubscriber('/follower/odom');
+followerScan = rossubscriber('/follower/scan');
+guiderControl = rospublisher('/guider/cmd_vel', 'geometry_msgs/Twist');
+
+controlOutput = rosmessage('geometry_msgs/Twist');
+controlOutput.Linear.X = 0.5;
+
+send(guiderControl, controlOutput);
+
+
+% rosshutdown;
