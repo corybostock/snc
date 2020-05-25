050862873b1945e6b63faa2f0689759dac5bc5d7
User <cory.bostock@gmail.com>
2020-05-25 22:25:42 +1000

    committing progress

--- turtleBotMain.m
+++ turtleBotMain.m
@@ -1,18 +1,16 @@
 %% Authors Cory Bostock Sabrina Emanouel
 % Prep the workspace
 close all
-set(0,'DefaultFigureWindowStyle','docked')
 clc
-clf
 clear
 
 % Init ROS
-% rosshutdown;
-% rosinit;                                                                     % makes a ROS node and allows comms to ROS
-% rostopic list;
-% disp('Run roslaunch files in order, then continue the Matlab script: ')
-% disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
-% input('Press enter to continue after running the roslaunch file: ');
+rosshutdown;
+rosinit;                                                                     % makes a ROS node and allows comms to ROS
+rostopic list;
+disp('Run roslaunch files in order, then continue the Matlab script: ')
+disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
+input('Press enter to continue after running Gazebo: ');
 
 % Making robot instances, subscribing and publishing ROS topics
 guider      = robot('/guider/odom', '/guider/scan', '/guider/cmd_vel');
@@ -24,30 +22,30 @@ disp('Moving guider: ');
 motion.circle(guider, 1.0, 0.5);
 input('Press enter to move to next stage: ');
 motion.stop(guider);
-motion.stop(follwer);
+motion.stop(follower);
 pause(0.7);
 
 disp('Moving guider: ');
 motion.circle(guider, 1.5, -1.0);
 input('Press enter to move to next stage: ');
 motion.stop(guider);
-motion.stop(follwer);
+motion.stop(follower);
 pause(0.7);
 
 disp('Moving guider: ');
 motion.circle(guider, 1.5, 1.0);
 input('Press enter to move to next stage: ');
 motion.stop(guider);
-motion.stop(follwer);
+motion.stop(follower);
 pause(0.7);
 
 disp('Moving guider: ');
 motion.circle(guider, -1.0, -0.5);
 input('Press enter to move to next stage: ');
 motion.stop(guider);
-motion.stop(follwer);
+motion.stop(follower);
 pause(0.7);
 
 disp('End of demonstration')
 
-% rosshutdown;
+rosshutdown;
