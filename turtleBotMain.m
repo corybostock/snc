5cdfab3782af5d1e674a5306c95b25db26063697
corybostock <cory.bostock@gmail.com>
2020-06-06 17:57:13 +1000

    modified waffle xacro. updated the main

--- turtleBotMain.m
+++ turtleBotMain.m
@@ -4,6 +4,9 @@ close all
 clc
 clear
 
+% For this project make sure the Turtlebot model is set to Waffle in your
+% ~./bashrc file
+
 % Init ROS
 rosshutdown;
 rosinit;                                                                     % makes a ROS node and allows comms to ROS
@@ -13,9 +16,9 @@ disp('roslaunch turtlebot3_gazebo multi_turtlebot3.launch ');
 input('Press enter to continue after running Gazebo: ');
 
 % Making robot instances, subscribing and publishing ROS topics
-% guider      = robot('/guider/odom', '/guider/scan', '/guider/cmd_vel', '/guider/mybot/camera1/image_raw');
+% guider      = robot('/guider/odom', '/guider/scan', '/guider/cmd_vel', '/guider/camera/rgb/image_raw');
 guider      = robot('/guider/odom', '/guider/scan', '/guider/cmd_vel');
-% follower    = robot('/follower/odom', '/follower/scan', '/follower/cmd_vel', '/follower/mybot/camera1/image_raw');
+% follower    = robot('/follower/odom', '/follower/scan', '/follower/cmd_vel', '/follower/camera/rgb/image_raw');
 follower    = robot('/follower/odom', '/follower/scan', '/follower/cmd_vel');
 motion      = move();
 
@@ -41,13 +44,6 @@ motion.stop(guider);
 motion.stop(follower);
 pause(0.7);
 
-disp('Moving guider: ');
-motion.circle(guider, -1.0, -0.5);
-input('Press enter to move to next stage: ');
-motion.stop(guider);
-motion.stop(follower);
-pause(0.7);
-
 disp('End of demonstration')
 
-rosshutdown;
+% rosshutdown;
