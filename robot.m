ba989ef40a8601c06d886837dbc7bdf9227777a7
corybostock <cory.bostock@gmail.com>
2020-06-06 15:45:08 +1000

    code compiles again now as a safe point of restore. The motion of the turtlebots still works. The follower needs to be moved before the final assignment demo.

--- robot.m
+++ robot.m
@@ -7,11 +7,18 @@ classdef robot < handle
     end
     
     methods
-        function obj = robot(odom,scan,cmd_vel, cam)
+%         function obj = robot(odom,scan,cmd_vel, cam)
+%             % Subscribing and publishing ROS topics
+%             obj.odom    = rossubscriber(odom);
+%             obj.scan    = rossubscriber(scan);
+%             obj.camera  = rossubscriber(cam);
+%             obj.cmd_vel = rospublisher(cmd_vel, 'geometry_msgs/Twist');
+%         end
+        
+        function obj = robot(odom,scan,cmd_vel)
             % Subscribing and publishing ROS topics
             obj.odom    = rossubscriber(odom);
             obj.scan    = rossubscriber(scan);
-            obj.camera  = rossubscriber(cam);
             obj.cmd_vel = rospublisher(cmd_vel, 'geometry_msgs/Twist');
         end
         
